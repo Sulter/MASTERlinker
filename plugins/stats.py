24e5552425aa2a502b77f3964850d5ac96bc79a7
Sulter <sulter.marhorn@gmail.com>
2013-11-18 15:45:50 +0100

    (plugin) stats, cleanup, feature
    added "random quote", some cleaning

--- plugins/stats.py
+++ plugins/stats.py
@@ -4,6 +4,7 @@ import sqlite3
 import settings
 import time
 import re
+import random
 
 class stats():
     def stats(self, main_ref, msg_info):
@@ -14,10 +15,6 @@ class stats():
         #count up the number of lines and words
         self.add_line_n_words(msg_info["nick"], msg_info["message"])
         
-    def update_req_time(self, nick):
-        self.cursor.execute("UPDATE nickstats SET time_last_req=? WHERE nickname=?", (int(time.time()), nick))
-        self.connection.commit()
-
     def add_line_n_words(self, nick, msg):
         self.cursor.execute("INSERT OR IGNORE INTO nickstats(nickname, init_time) VALUES(?,?)", (nick, int(round(time.time(),0))))
         self.cursor.execute("UPDATE nickstats SET lines = lines + 1 WHERE nickname=?", (nick,))
@@ -25,11 +22,14 @@ class stats():
         words = len(re.findall("\S+", msg))
         self.cursor.execute("UPDATE nickstats SET words = words + ? WHERE nickname=?", (words, nick))
         self.connection.commit()
+        #there is x chance that this phrase will be added as that persons "quote"
+        if random.random() > 0.95:
+            self.cursor.execute("UPDATE nickstats SET random_quote=? WHERE nickname=?", (msg, nick))
         
     def __init__(self):
         db_path="plugins/stats.db"
         self.connection = sqlite3.connect(db_path)
         self.cursor = self.connection.cursor()
         #we first establish the table
-        self.cursor.execute('CREATE TABLE if not exists nickstats (Id INTEGER PRIMARY KEY, nickname TEXT UNIQUE, lines INT DEFAULT(0), words INT DEFAULT(0), init_time INT)')
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS nickstats (Id INTEGER PRIMARY KEY, nickname TEXT UNIQUE, lines INT DEFAULT(0), words INT DEFAULT(0), init_time INT, random_quote TEXT)')
         self.connection.commit()
