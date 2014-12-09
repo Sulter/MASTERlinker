a3794f50fba1c2dee611309c01ab3f46a5f1f9c7
Justin <justinhughart@gmail.com>
2014-12-08 18:30:12 -0600

    -Updated it to Sulter's standards.

--- plugins/roll.py
+++ plugins/roll.py
@@ -1,38 +1,28 @@
+# A plugin that rolls a random number based on a user-defined range.
 from random import randrange
 
-
-
-#Rolls a random number.
-
 class roll():
-
-    
-
+	# Main function.
 	def roll(self, main_ref, msg_info):
-        
-		if not msg_info["message"].startswith("!roll"):
-            
+		# We'll guard against PMs.
+		if msg_info["channel"] == settings.NICK:
 			return None
-
-		
-
-		input = msg_info["message"].replace("!roll","")
-	
-
+		#We'll also ignore any calls not starting with !roll.
+		if not msg_info["message"].startswith("!roll"):
+			return None
+		#We'll get the string without the !roll call...
+		input = msg_info["message"].replace("!roll", "")
+		#If the string is not a number, we'll ignore the call.
 		if not self.is_number(input):
 			return None
-			
-		
-		response = str(randrange((int)(input)))		
-		
-		
+		#Generate a random number and output it.
+		response = str(randrange((int)(input)))
 		main_ref.send_msg(msg_info["channel"], response)
 
-
+	# Returns whether or not the string is a number based on if the cast errors.
 	def is_number(self, s):
-	    try:
-        	int(s)
-	        return True
-	    except ValueError:
-        	return False
-	
+		try:
+			int(s)
+			return True
+		except ValueError:
+			return False
